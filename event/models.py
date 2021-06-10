002e56fc2b630e812a07062d021b4398b6298fb3
Priya Nayak <priyanayak.cs19@rvce.edu.in>
2021-06-10 10:05:53 +0530

    Field names changed

--- event/models.py
+++ event/models.py
@@ -11,24 +11,33 @@ class CollegeName(models.Model):
     def __str__(self):
         return self.college_name
 
+class SME(models.Model):
+    sme_name=models.CharField(max_length=100)
+    sme_notes_id=models.CharField(max_length=100, verbose_name='SME Notes ID')
+    sme_manager_notes_id=models.CharField(max_length=100 , verbose_name='SME Manager Notes ID')
+
+    def __str__(self):
+        return self.sme_name
+
 
 class Event(models.Model):
     ambassodor = models.CharField(max_length=500, choices=BOOLEAN_OPTION, blank=True)
     date = models.DateField(blank=True, null=True)
-    Quarter =  models.IntegerField(default=4, null=True, blank=True)
-    EventActivityType = models.CharField(max_length=500, choices=EVENT_OPTION, null=True, blank=True)
-    TechnologyTracks = models.CharField(max_length=500, choices=TRACKS_OPTION, null=True, blank=True, verbose_name='Technology Tracks')
+    quarter =  models.IntegerField(default=4, null=True, blank=True)
+    event_activity_type = models.CharField(max_length=500, choices=EVENT_OPTION, null=True, blank=True)
+    technology_tracks = models.CharField(max_length=500, choices=TRACKS_OPTION, null=True, blank=True, verbose_name='Technology Tracks')
     event_activity_mode = models.CharField(max_length=500, choices=EVENT_MODE_OPTION, null=True, blank=True)
-    OrganisedBy = models.CharField(max_length=500, choices=ORGANISED_OPTION, null=True, blank=True)
+    organised_by = models.CharField(max_length=500, choices=ORGANISED_OPTION, null=True, blank=True)
     session_topic_name = models.CharField(max_length=100, blank=True, null=True)
     number_of_attendees = models.IntegerField(default=0)
     institution_name = models.ForeignKey(CollegeName, blank=True, null=True, on_delete=models.SET_NULL)
-    SessionDuration = models.CharField(max_length=500, choices=SESSION_OPTION, null=True, blank=True, help_text='Session Duration in hours.')
-    SMEBU = models.CharField(max_length=500, choices=BU_OPTION, null=True, blank=True)
-    URSPOC = models.CharField(max_length=500, choices=URSPOC_OPTION, null=True, blank=True)
+    sme_details= models.ForeignKey(SME, blank=True, null=True, on_delete=models.SET_NULL)
+    session_duration = models.CharField(max_length=500, choices=SESSION_OPTION, null=True, blank=True, help_text='Session Duration in hours.')
+    sme_bu = models.CharField(max_length=500, choices=BU_OPTION, null=True, blank=True,verbose_name='SME BU')
+    ur_spoc = models.CharField(max_length=500, choices=URSPOC_OPTION, null=True, blank=True,verbose_name='UR SPOC')
     link = models.URLField(null=True, blank=True, help_text='Link of Academic Initiative Course/platform to be used from  https://ibm.biz/academic')
-    Status = models.CharField(max_length=500, choices=STATUS_OPTION, null=True, blank=True)
-    CollegeCategory = models.CharField(max_length=500, choices=COLLEGE_OPTION, null=True, blank=True)
+    status = models.CharField(max_length=500, choices=STATUS_OPTION, null=True, blank=True)
+    college_category = models.CharField(max_length=500, choices=COLLEGE_OPTION, null=True, blank=True)
 
 
     @property
