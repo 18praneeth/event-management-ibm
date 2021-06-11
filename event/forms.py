fe41dbd6db938134dfcba924b1bcc8658ca5f622
Praneeth <18praneeth@gmail.com>
2021-06-11 22:18:12 +0530

    Updated forms.py

--- event/forms.py
+++ event/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth import models
 from django.db.models.base import Model
 from django.forms import fields
-from .models import Comments, Event
+from .models import Comments, Event, SME
+from django.utils.translation import gettext_lazy
 
 
 class CommentForm(forms.ModelForm):
@@ -15,3 +16,15 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = '__all__'
+
+
+class SMEForm(forms.ModelForm):
+    class Meta:
+        model = SME
+        fields = ['sme_name', 'sme_notes_id', 'sme_manager_notes_id']
+        labels = {
+            'sme_name': gettext_lazy('SME Name')
+        }
+        help_texts = {
+            'sme_name': gettext_lazy('Please enter SME Name')
+        }
