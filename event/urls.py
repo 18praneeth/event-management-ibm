fc700a6466450c8fe51a33328f80facf9a892d1a
Praneeth <18praneeth@gmail.com>
2021-06-18 22:16:37 +0530

    college edit and create

--- event/urls.py
+++ event/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     path('detail/<int:event_id>', event_detail, name='event-detail'),
     path('detail/event-delete/<int:event_id>', event_delete, name='event-delete'),
     path('detail/event-update/<int:event_id>', event_update, name='event-update'),
-    path('college/',create_college,name='college'),
+    path('college/',create_college,name='college-create'),
     path('create-event', create_event, name='create-event'),
+    path('college-details',college_details,name='college-details'),
+    path('college-edit/<int:id>', college_edit, name='college-edit'),
     path('accept-event/<int:id>', signup_event, name='signup-event'),
     path('reject-event/<int:id>', reject_event, name='reject-event'),
     path('signup-event/<int:id>', mail_signup, name='mail-signup'),
-    path('college-details',college_details,name='college-details'),
     path('event-assign/<int:id>', event_control, name='event-assign')
 ]
